443a10e34d39394a9307a179814f54def89fb7bb
vmduque <duquevasquezvictormanuel@gmail.com>
2025-10-04 08:56:43 -0500

    Presencial 09

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -5,7 +5,7 @@ def load_data():
 
     import pandas as pd
 
-    dataset = pd.read_csv("auto_mpg.csv")
+    dataset = pd.read_csv("files/input/auto_mpg.csv")
     dataset = dataset.dropna()
     dataset["Origin"] = dataset["Origin"].map(
         {1: "USA", 2: "Europe", 3: "Japan"},
@@ -21,9 +21,9 @@ def load_estimator():
     import os
     import pickle
 
-    if not os.path.exists("estimator.pickle"):
+    if not os.path.exists("homework/estimator.pickle"):
         return None
-    with open("estimator.pickle", "rb") as file:
+    with open("homework/estimator.pickle", "rb") as file:
         estimator = pickle.load(file)
 
     return estimator
@@ -31,14 +31,14 @@ def load_estimator():
 
 def test_01():
 
-    from sklearn.metrics import accuracy_score
+    from sklearn.metrics import r2_score
 
     x, y = load_data()
     estimator = load_estimator()
 
-    accuracy = accuracy_score(
-        y_true=y,
-        y_pred=estimator.predict(x),
+    r2 = r2_score(
+        y, 
+        estimator.predict(x),
     )
 
-    assert accuracy > 0.9545
+    assert r2 > 0.6
